3eee85ccf1fc9b9d2e28dfd84ae8ee287231c16b
MasoudGit <masoud@localhost.com>
2016-02-14 22:46:36 +0330

    init

---
+++ Assets/Scripts/Sections/_SectionController/SectionChooser.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SectionChooser : MonoBehaviour {
+
+    public static int loadSectionIndex= 200;
+    public bool load;
+
+    public InputField inputMission;
+    
+    public void loadSection()
+    {
+        int num;
+        bool itsRight = int.TryParse( inputMission.text.ToString() , out num ) ;
+        if (itsRight)
+            loadSectionIndex = num;
+        else
+            loadSectionIndex = 0;
+
+        Application.LoadLevel("Game");
+    }
+}
